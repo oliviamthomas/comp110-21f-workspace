6881ec7160b8ad9cdc3c31a76d360ac851c55f73
Olivia Thomas <othomas1@live.unc.edu>
2021-09-07 11:08:47 -1000

    Lesson 10 Practice Questions

---
+++ lessons/ls10_warmup1.py
@@ -0,0 +1,13 @@
+"""Challenge Question #1!"""
+
+choice: int = int(input("Enter a number: "))
+
+if choice > 50: 
+    if choice < 25:
+        print("A")
+    else:
+        print("B")
+else:
+    print("C")
+    if choice > 75:
+        print("D")
