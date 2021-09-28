83b72e9676ad10f186529a3fb2bc906029f7188a
Olivia Thomas <othomas1@live.unc.edu>
2021-09-28 14:52:40 -0400

    EX04 start

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,17 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730225468"
 
 
-# TODO: Implement your functions here.
+def all(x: list[int], y: int) -> bool:
+    """A list of ints that will result in True if all int values are the same."""
+    length: int = len(x)
+    y: int = 1
+    if length == y:
+        return True
+    else:
+        return False
+
+
+x: list[int] = [1, 5]
+print(x)
