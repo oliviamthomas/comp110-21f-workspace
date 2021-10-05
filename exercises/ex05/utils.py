e1a1a52a70feb55232f49ebfaeff8c5ab98bbaf7
Olivia Thomas <othomas1@live.unc.edu>
2021-10-04 22:39:40 -0400

    ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,52 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730225468"
 
-# Define your functions below
+
+def only_evens(list1: list[int]) -> list[int]:
+    """Find only the even elements within a list."""
+    i: int = 0
+    list2: list[int] = []
+
+    while i < len(list1):
+        if list1[i] % 2:
+            list2.append(list1[i])
+        i += 1
+    return list2
+
+
+print(only_evens([1, 2, 3, 4, 5]))
+
+
+def sub(list1: list[int]) -> list[int]:
+    """Subset of a given list."""
+    i_start: int = 0
+    i_end: int = 10
+
+    while i_start < len(list1):
+        if len(list1) < i_end:
+            list1[i_start] += 1
+            i_start += 2
+        else:
+            return list1
+    return list1
+
+
+print(sub([1, 2, 4, 15]))
+
+
+def concat(list1: list[int], list2: list[int]) -> list[int]:
+    """Two lists that will come together."""
+    both_lists: list[int] = []
+    i: int = 0
+    i2: int = 0
+    while i < 4:
+        list1.append(i)
+        i += 1
+    while i2 < 4:
+        list2.append(i2)
+        i2 += 2
+    return both_lists
+
+
+print(concat([1, 2, 3, 4], [5, 6, 7, 8]))
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,22 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+from exercises.ex05.utils import only_evens, sub, concat
+
+__author__ = "730225468"
+
+
+def test_only_evens() -> None:
+    list1: list[int] = []
+    assert only_evens(list1) == []
+
+
+def test_sub() -> None:
+    list1: list[int] = []
+    assert sub(list1) == []
+
+
+def test_concat() -> None:
+    list1: list[int] = []
+    list2: list[int] = []
+    assert concat(list1, list2) == []
