bd92db27394ef79f4c2ad91e775253eee9a4f6f6
Olivia Thomas <othomas1@live.unc.edu>
2021-10-14 12:26:55 -0400

    LS19 in class work

---
+++ lessons/ls19_challenge1.py
@@ -0,0 +1,22 @@
+"""Helper functions imported elsewhere."""
+
+
+def main() -> None:
+    game0: dict[str, int] = {"KJ": 0, "ML": 1}
+    game1: dict[str, int] = {"ML": 2, "EW": 3}
+    merged: dict[str, int] = merge(game0, game1)
+    print(merged)
+
+
+def merge(a: dict[str, int], b: dict[str, int]) -> dict[str, int]:
+    """Merge two dictionaries."""
+    result: dict[str, int] = {}
+    for key in a:
+        result[key] = a[key]
+    for key in b:
+        result[key] = b[key]
+    return result
+
+
+if __name__ == "__main__":
+    main()
