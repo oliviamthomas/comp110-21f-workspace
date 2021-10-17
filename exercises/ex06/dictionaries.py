b41d326b8eb58984b166b3e29f812928f777d024
Olivia Thomas <othomas1@live.unc.edu>
2021-10-17 19:57:47 -0400

    EX06 work

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,59 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730225468"
 
-# Define your functions below
+
+def main() -> None:
+    print(invert({"Olivia": "Colton", "Emily": "Bryan"}))
+    print(favorite_color({"Olivia": "Blue", "Colton": "Blue", "Emily": "Pink", "Bryan": "Green"}))
+    print(count(["Olivia", "Colton", "Olivia", "Bryan"]))
+
+
+def invert(a: dict[str, str]) -> dict[str, str]:
+    """Invert keys and values in a dictionary."""
+    result: dict[str, str] = {}
+    for key in a:
+        value = a[key]
+        if value in result:
+            raise KeyError("The two keys cannot be the same")
+        result[value] = key
+    
+    return result
+
+
+def favorite_color(input: dict[str, str]) -> str:
+    """Results in the color that appears most frequently."""
+    color_counts: dict[str, int] = {}
+    for person in input:
+        color = input[person]
+        if color not in color_counts:
+            color_counts[color] = 1 
+            # adding new row
+        else: 
+            color_counts[color] += 1 
+            # adding one to the value of the row that is already there
+    maximum: int = 0
+    # lowest possible number 
+    winning_color: str = ""
+    for key in color_counts:
+        if color_counts[key] > maximum:
+            maximum = color_counts[key]
+            winning_color = key
+
+    return winning_color
+
+
+def count(x: list[str]) -> dict[str, int]:
+    result: dict[str, int] = {}
+    item: str = ""
+    for item in x:
+        if item not in result:
+            result[item] = 1
+        else:
+            result[item] += 1
+
+    return result
+
+
+if __name__ == "__main__":
+    main()
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,59 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730225468"
+
+
+def test_invert_edge() -> None:
+    """Edge test for invert."""
+    a: dict[str, str] = {}
+    assert invert(a) == {}
+
+
+def test_invert_use1() -> None:
+    """Use test 1 for invert."""
+    a: dict[str, str] = {"Olivia": "Colton", "Emily": "Bryan"}
+    assert invert(a) == {"Colton": "Olivia", "Bryan": "Emily"}
+
+
+def test_invert_use2() -> None:
+    """Use test 2 for invert."""
+    a: dict[str, str] = {"Pepperoni": "Pizza", "Peanut butter": "Jelly"}
+    assert invert(a) == {"Pizza": "Pepperoni", "Jelly": "Peanut butter"}
+
+
+def test_favorite_color_edge() -> None:
+    """Edge test for favorite color."""
+    input: dict[str, str] = {}
+    assert favorite_color(input) == ""
+
+
+def test_favorite_color_use1() -> None:
+    """Use test 1 for favorite color."""
+    input: dict[str, str] = {"Olivia": "Blue", "Colton": "Blue", "Emily": "Pink", "Bryan": "Green"}
+    assert favorite_color(input) == "Blue"
+
+
+def test_favorite_color_use2() -> None:
+    """Use test 2 for favorite color."""
+    input: dict[str, str] = {"Olivia": "Red", "Colton": "Yellow", "Emily": "Red", "Bryan": "Yellow"}
+    assert favorite_color(input) == "Red"
+
+
+def test_count_edge() -> None:
+    """Edge test for count."""
+    x: list[str] = []
+    assert count(x) == {}
+
+
+def test_count_use1() -> None:
+    """Use test 1 for count."""
+    x: list[str] = ["Olivia", "Colton", "Olivia", "Bryan"]
+    assert count(x) == {"Olivia": 2, "Colton": 1, "Bryan": 1}
+
+
+def test_count_use2() -> None:
+    """Use test 2 for count."""
+    x: list[str] = ["Emily", "Olivia", "Emily", "Emily"]
+    assert count(x) == {"Emily": 3, "Olivia": 1}
